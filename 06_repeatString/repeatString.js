1b4f0b27bff2291bf09d233d74dc82bcf6e8a97e
santikapelo2002 <santikapelo@gmail.com>
2025-07-05 22:16:18 +0200

    EJercicio 6 hecho

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string,number) {
+    let contador=0;
+    let resultado = "";
+    if(number<0){
+        return "ERROR"
+    }
+while(contador!=number){
+     contador++;
+     resultado+=string;
+   
+   
+}
+return resultado;
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
