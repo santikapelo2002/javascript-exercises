b86036e8fbc0fed5260c00b2c10f828171f4593f
santikapelo2002 <santikapelo@gmail.com>
2025-07-06 20:38:14 +0200

    ejercicio 7 hecho

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+let resultado ="";
+for(let i=string.length-1;i>=0;i--){
+    resultado+=string[i];
+}
+return resultado;
 };
-
+console.log(reverseString("calabaza"))
 // Do not edit below this line
 module.exports = reverseString;
