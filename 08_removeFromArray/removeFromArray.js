1ba9960103eb3e9dfcf0ad02bcfe9dff7a8015c8
santikapelo2002 <santikapelo@gmail.com>
2025-07-06 21:56:18 +0200

    Ejercicio 8 hecho

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...number) {
+    return array.filter(array=>!number.includes(array));
 };
 
 // Do not edit below this line
