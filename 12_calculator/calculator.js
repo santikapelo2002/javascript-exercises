70daa8768f93e627213150845760802d5df3b224
santikapelo2002 <santikapelo@gmail.com>
2025-07-18 21:38:43 +0200

    ejercicio calculadora

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return Math.floor(num1+num2);
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const subtract = function(num1,num2) {
+ 
+  
+  
+		return Math.floor(num1-num2);
 
 };
 
-const power = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc,current)=>{return acc+=current},0)
 };
 
-const factorial = function() {
-	
+const multiply = function(arr) {
+			return arr.reduce((acc,current)=>{return acc*=current},1)
+
+
+};
+
+const power = function(base,exponente) {
+	return Math.floor(base**exponente);
+};
+
+const factorial = function(num) {
+  if(num<0) return 'Error';
+  
+  if(num===0)return 1;
+  
+  let resultado=1;
+	for(let i=num;i>0;i--){
+    resultado*=i;
+  }
+  return resultado;
 };
 
 // Do not edit below this line
