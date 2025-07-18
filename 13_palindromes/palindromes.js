91a8d33fb38e4e1c8f07bf129384f154585b634e
santikapelo2002 <santikapelo@gmail.com>
2025-07-18 21:52:41 +0200

    ejercicio aún por acabar, falta eliminar espacios en blanco de dentro del string y sus palabras y eliminar comas y caracteres especiales

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+   let stringNuevo = string.split("").slice().reverse().join("").toLowerCase().trim();
+   let stringFinal = string.toLowerCase().trim();
+  
+if(stringFinal==stringNuevo){
+    return true;
+}else{
+    return false;
+}
 };
 
 // Do not edit below this line
