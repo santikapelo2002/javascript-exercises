fd360f663cd9a34f7bed900d2c912ca78d1f7b2d
santikapelo2002 <santikapelo@gmail.com>
2025-07-19 03:52:44 +0200

    ejercicio fibonacci resuelto con un bucle que itera la cantidad de veces según el número que pasa el usuario como argumento y pasa el dígito de la secuencia equivalente a la posición del argumento de la función

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num=parseInt(num);
+    if(isNaN(num)){return 'Error';}
+    if(num==0)return parseInt(num);
+    if(num<0)return "OOPS";
+   
+
+    let array=[];
+    let a=1;
+    let b=1;
+    array.push(a,b);
+    for(let i=0;i<=num-2;i++){
+        let resultado = a+b;
+        array.push(resultado);
+        b=a;
+        a=resultado;
+    }
+    return array[num-1];
 
 };
 
