d2936b342fd0373b49c8a3f31247c01cd29e56ee
santikapelo2002 <santikapelo@gmail.com>
2025-07-19 04:45:36 +0200

    ejercicio resuelto utilizando reduce y comparando años con el acumulador

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  const currentYear = new Date().getFullYear();
 
+  let resultado = arr.reduce((acc, current) => {
+    let accYears = (acc.yearOfDeath || currentYear) - acc.yearOfBirth;
+    let currentYears = (current.yearOfDeath || currentYear) - current.yearOfBirth;
+
+    return accYears > currentYears ? acc : current;
+  });
+
+  return resultado;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
